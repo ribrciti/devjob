8224f99ee18e43db1fabdd3f237ac66ea4be91bc
ribrciti <rbciti@msn.com>
2020-12-27 13:23:46 -0800

    Added job resource and action text

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,6 +35,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :jobs, dependent: :destroy
+
 
   def name
     "#{first_name} #{last_name}"
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  resources :jobs
   devise_for :users
+
+  authenticate :user, lambda { |u| u.admin? } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
+
+  mount SimpleDiscussion::Engine => "/forum"
 
   get 'home/index'
 
   root to:  "home#index"
-
-  mount Sidekiq::Web => '/sidekiq'
-
-  mount SimpleDiscussion::Engine => "/forum"
 
 end
